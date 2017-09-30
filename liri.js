a33e784fa31903a2b487cc313562cf7d495b5716
Adam Champagne <adamchampagne@gmail.com>
2017-09-30 08:37:00 -0400

    spotify module working

--- liri.js
+++ liri.js
@@ -38,6 +38,52 @@ if (input == "my-tweets") {
  * SPOTIFY SECTION
 **************************/
 
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+  id: "e78e58e70d0946a29e207d49558331fd",
+  secret: "3f9db0fed71c4415a7307566deb0ffac",
+});
+
+var nodeArgs = process.argv;
+var songName = "";
+
+for (var i = 2; i < nodeArgs.length; i++){
+  if (i > 2 && i < nodeArgs.length){
+    songName = songName + "+" + nodeArgs[i];
+  } else {
+    songName += nodeArgs[i];
+  }
+}
+
+
+
+
+spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, songData) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+ 
+  for (var key in songData){
+    // console.log(songData[key].items);
+    var items = songData[key].items;
+    for (var key in items) {
+      console.log("---------------------------------");
+      
+      console.log("Artist: " + items[key].artists[key].name);
+      console.log("Song Name: " + songName);
+      console.log("Album: " + items[key].album.name);
+      console.log("Spotify Link: " + items[key].album.href);
+
+      console.log("---------------------------------");
+      
+      
+    }
+  }
+ 
+});
+
+
 var fs = require("fs");
 fs.readFile("random.txt", "utf8", function(err, data){
   if(err){
